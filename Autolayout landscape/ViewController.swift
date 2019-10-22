ec972cdf5b8e629b60f696b1889cf5371229fd93
studentp_10 <IamSid25>
2019-10-22 13:20:55 +0530

    Initial Commit

---
+++ Autolayout landscape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout landscape
+//
+//  Created by Student P_10 on 22/10/19.
+//  Copyright © 2019 felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
